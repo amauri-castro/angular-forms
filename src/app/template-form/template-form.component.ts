a99f97ec47dcae29735373ab4b00fef652f9f823
amauri-castro <amauri.p.castro@gmail.com>
2024-04-17 22:36:29 -0300

    atualizacao dos dados no formulario usando patchValue

--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template-form',
@@ -21,18 +22,25 @@ export class TemplateFormComponent implements OnInit {
   onSubmit(form: any){
     console.log(form)
     //console.log(this.usuario)
+
+    this.http.post('enderecoServer/formUsuario', JSON.stringify(form.value))
+      .subscribe( dados => console.log(dados))
   }
 
-  consultaCEP(cep: string){
+  consultaCEP(cep: string, form: NgForm){
     cep = cep.replace(/\D/g, '');
     console.log(cep)
     if(cep){
       var validacep = /^[0-9]{8}$/;
       if(validacep.test(cep)) {
-        this.http.get(`https://viacep.com.br/ws/${cep}/json`).subscribe(
+        this.resetarFormulario(form);
+        this.http.get(`https://viacep.com.br/ws/${cep}/json`)
+        .subscribe(
           dados => {
-            console.log(dados);
-          }
+            this.populaDadosForm(dados, form)
+            console.log(dados)
+          } 
+            
         );
       }
     } else {
@@ -40,11 +48,50 @@ export class TemplateFormComponent implements OnInit {
     }
   }
 
+  populaDadosForm(dados:any, formulario: NgForm){
+    /*
+    formulario.setValue({
+      nome: formulario.value.nome,
+      email: formulario.value.email,
+      endereco: {
+        rua: dados.localidade,
+        cep: dados.cep,
+        numero: '',
+        complemento: '',
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf,
+        
+
+      }
+    });*/
+    formulario.form.patchValue({
+      endereco: {
+        rua: dados.logradouro,
+        complemento: dados.complemento,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf,
+      }
+    });
+    //console.log(formulario)
+  }
+
   verificaValidTouched(campo: any){
     return !campo.valid && campo.touched;
   }
 
-
+  resetarFormulario(formulario: NgForm){
+    formulario.form.patchValue({
+      endereco: {
+        rua: null,
+        complemento: null,
+        bairro: null,
+        cidade: null,
+        estado: null,
+      }
+    });
+  }
   aplicaCssErro(campo: any){
     return {
       'was-validated': this.verificaValidTouched(campo)
